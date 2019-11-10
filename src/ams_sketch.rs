0ad2d21fea1c28cd28daa3d752ef730b7228558e
Vesa Vilhonen <vesa@vilhonen.com>
2019-11-10 21:24:51 +0200

    Reorganize

--- src/ams_sketch.rs
+++ src/ams_sketch.rs
@@ -53,3 +53,57 @@ fn hash(key: u32, item: usize) -> u32 {
     let data: [u8; 4] = unsafe { ::std::mem::transmute(item as u32) };
     murmurhash3_x86_32(&data, key)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::ams_sketch::AMSSketch;
+
+    struct Tester(HashMap<usize, usize>, u32);
+
+    impl Tester {
+        pub fn new(k: u32) -> Self {
+            Tester(HashMap::new(), k)
+        }
+
+        pub fn update(&mut self, i: usize) {
+            let entry = self.0.entry(i).or_insert(0);
+            *entry += 1;
+        }
+
+        pub fn estimate(&self) -> f32 {
+            self.0.values().map(|val| val.pow(self.1)).sum::<usize>() as f32
+        }
+    }
+
+    #[test]
+    fn test() {
+        let (lambda, epsilon) = (0.5, 0.01);
+
+        let count = 100;
+        let mut runs = Vec::with_capacity(count);
+        for _ in 0..count {
+            let mut sketch = AMSSketch::new(2, 100, lambda, epsilon);
+            let mut tester = Tester::new(2);
+
+            for i in 0..100 {
+                for _ in 0..10 {
+                    sketch.update(i);
+                    tester.update(i);
+                }
+            }
+            let ams_est = sketch.estimate();
+            let det_est = tester.estimate();
+            runs.push((ams_est, det_est));
+            //        println!("DELTA: {} AMS sketch: {} Tester: {}", (ams_est - det_est).abs(), ams_est, det_est);
+        }
+        let median_error = {
+            let mut runs = runs.iter().map(|(ams, det)| (ams - det).abs() as u32).collect::<Vec<_>>();
+            runs.sort();
+            runs[runs.len() / 2]
+        };
+
+        println!("{} runs with ε = {}, λ = {}", count, epsilon, lambda);
+        println!("median error {}", median_error);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+mod ams_sketch;
