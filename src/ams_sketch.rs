e2a00d2bd540d3401b3d6ebd219ab678506a263a
Vesa Vilhonen <vesa@vilhonen.com>
2019-11-11 11:47:52 +0200

    Do multiple runs

--- src/ams_sketch.rs
+++ src/ams_sketch.rs
@@ -1,22 +1,39 @@
+#![allow(deprecated)]
 use murmurhash3::murmurhash3_x86_32;
 use rand::random;
 
-pub struct AMSSketch {
-    k: u32,
-    data: Vec<Vec<(u32, isize)>>,
+type Seed = u32;
+
+fn create_seed() -> Seed {
+    random()
 }
 
+fn hash(key: Seed, item: usize) -> u32 {
+    let data: [u8; 4] = unsafe { ::std::mem::transmute(item as u32) };
+    murmurhash3_x86_32(&data, key)
+}
+
+pub struct AMSSketch {
+    k: u32,
+    data: Vec<Vec<(Seed, isize)>>,
+}
+
+#[allow(dead_code)]
 impl AMSSketch {
     pub fn new(k: u32, n: usize, lambda: f32, epsilon: f32) -> Self {
+        let (s1, s2) = Self::attributes(k, n, lambda, epsilon);
+        let data = (0..s2)
+            .map(|_| (0..s1).map(|_| (create_seed(), 0)).collect())
+            .collect();
+        AMSSketch { k, data }
+    }
+
+    pub fn attributes(k: u32, n: usize, lambda: f32, epsilon: f32) -> (usize, usize) {
         let kf = k as f32;
         let n = n as f32;
         let s1 = (8.0 * kf * n.powf(1.0 - 1.0 / kf) / lambda.powi(2)).ceil() as usize;
         let s2 = (2.0 * (1.0 / epsilon).ln()).ceil() as usize;
-        //        println!("k {} n {} lambda {} epsilon {} -> s1 {} s2 {}", k, n, lambda, epsilon, s1, s2);
-        let data = (0..s2)
-            .map(|_| (0..s1).map(|_| (random(), 0)).collect())
-            .collect();
-        AMSSketch { k, data }
+        (s1, s2)
     }
 
     pub fn update(&mut self, index: usize) {
@@ -49,15 +66,13 @@ impl AMSSketch {
     }
 }
 
-fn hash(key: u32, item: usize) -> u32 {
-    let data: [u8; 4] = unsafe { ::std::mem::transmute(item as u32) };
-    murmurhash3_x86_32(&data, key)
-}
-
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
     use crate::ams_sketch::AMSSketch;
+    use std::collections::HashMap;
+    use std::fs::File;
+    use std::io::Write;
+    use std::error::Error;
 
     struct Tester(HashMap<usize, usize>, u32);
 
@@ -77,13 +92,18 @@ mod tests {
     }
 
     #[test]
-    fn test() {
-        let (lambda, epsilon) = (0.5, 0.01);
+    fn test() -> Result<(), Box<dyn Error>> {
+        test_impl(2, 1, 0.1, 0.1)?;
+        test_impl(2, 1, 0.2, 0.1)?;
+        Ok(())
+    }
 
-        let count = 100;
+    fn test_impl(k: u32, n: usize, lambda: f32, epsilon: f32) -> Result<(), Box<dyn Error>> {
+        let count = 500;
         let mut runs = Vec::with_capacity(count);
+        let (s1, s2) = AMSSketch::attributes(k, n, lambda, epsilon);
         for _ in 0..count {
-            let mut sketch = AMSSketch::new(2, 100, lambda, epsilon);
+            let mut sketch = AMSSketch::new(k, n, lambda, epsilon);
             let mut tester = Tester::new(2);
 
             for i in 0..100 {
@@ -92,18 +112,38 @@ mod tests {
                     tester.update(i);
                 }
             }
-            let ams_est = sketch.estimate();
-            let det_est = tester.estimate();
-            runs.push((ams_est, det_est));
-            //        println!("DELTA: {} AMS sketch: {} Tester: {}", (ams_est - det_est).abs(), ams_est, det_est);
+            let ams_estimate = sketch.estimate();
+            let actual = tester.estimate();
+            runs.push((ams_estimate, actual));
         }
         let median_error = {
-            let mut runs = runs.iter().map(|(ams, det)| (ams - det).abs() as u32).collect::<Vec<_>>();
+            let mut runs = runs
+                .iter()
+                .map(|(ams, det)| (ams - det).abs() as u32)
+                .collect::<Vec<_>>();
             runs.sort();
             runs[runs.len() / 2]
         };
 
-        println!("{} runs with ε = {}, λ = {}", count, epsilon, lambda);
-        println!("median error {}", median_error);
+        let error_proportion = runs
+            .iter()
+            .filter(|(ams, det)| {
+                let error_dist = det * lambda;
+                (ams - det).abs() > error_dist
+            })
+            .count()
+            / runs.len();
+
+        {
+            let mut output = File::create(format!("test_results/k_{}_n_{}_lambda_{}_epsilon_{}.csv", k,n,lambda,epsilon))?;
+            writeln!(output, "estimate,actual")?;
+            for (ams_estimate, actual) in runs {
+                writeln!(output, "{},{}", ams_estimate, actual)?;
+            }
+        }
+
+        println!("{} runs with ε = {}, λ = {} (buckets = {}, copies = {})", count, epsilon, lambda, s1, s2);
+        println!("median error {}, error proportion {}", median_error, error_proportion);
+        Ok(())
     }
 }
