53725468aa32c58657297bbb45508b1547b4c84f
Vesa Vilhonen <vesa@vilhonen.com>
2019-11-11 16:19:02 +0200

    Remove unnecessary variable

--- src/ams_sketch.rs
+++ src/ams_sketch.rs
@@ -20,18 +20,16 @@ pub struct AMSSketch {
 
 #[allow(dead_code)]
 impl AMSSketch {
-    pub fn new(k: u32, n: usize, lambda: f32, epsilon: f32) -> Self {
-        let (s1, s2) = Self::attributes(k, n, lambda, epsilon);
+    pub fn new(k: u32, lambda: f32, epsilon: f32) -> Self {
+        let (s1, s2) = Self::attributes(k,lambda, epsilon);
         let data = (0..s2)
             .map(|_| (0..s1).map(|_| (create_seed(), 0)).collect())
             .collect();
         AMSSketch { k, data }
     }
 
-    pub fn attributes(k: u32, n: usize, lambda: f32, epsilon: f32) -> (usize, usize) {
-        let kf = k as f32;
-        let n = n as f32;
-        let s1 = (8.0 * kf * n.powf(1.0 - 1.0 / kf) / lambda.powi(2)).ceil() as usize;
+    pub fn attributes(k: u32, lambda: f32, epsilon: f32) -> (usize, usize) {
+        let s1 = (8.0 * k as f32 / lambda.powi(2)).ceil() as usize;
         let s2 = (2.0 * (1.0 / epsilon).ln()).ceil() as usize;
         (s1, s2)
     }
@@ -70,9 +68,9 @@ impl AMSSketch {
 mod tests {
     use crate::ams_sketch::AMSSketch;
     use std::collections::HashMap;
+    use std::error::Error;
     use std::fs::File;
     use std::io::Write;
-    use std::error::Error;
 
     struct Tester(HashMap<usize, usize>, u32);
 
@@ -93,18 +91,19 @@ mod tests {
 
     #[test]
     fn test() -> Result<(), Box<dyn Error>> {
-        test_impl(2, 1, 0.1, 0.1)?;
-        test_impl(2, 1, 0.2, 0.1)?;
+        test_impl(0.5, 0.1)?;
+        test_impl(0.6, 0.1)?;
         Ok(())
     }
 
-    fn test_impl(k: u32, n: usize, lambda: f32, epsilon: f32) -> Result<(), Box<dyn Error>> {
+    fn test_impl(lambda: f32, epsilon: f32) -> Result<(), Box<dyn Error>> {
         let count = 500;
         let mut runs = Vec::with_capacity(count);
-        let (s1, s2) = AMSSketch::attributes(k, n, lambda, epsilon);
+        let k = 2;
+        let (s1, s2) = AMSSketch::attributes(k, lambda, epsilon);
         for _ in 0..count {
-            let mut sketch = AMSSketch::new(k, n, lambda, epsilon);
-            let mut tester = Tester::new(2);
+            let mut sketch = AMSSketch::new(k, lambda, epsilon);
+            let mut tester = Tester::new(k);
 
             for i in 0..100 {
                 for _ in 0..10 {
@@ -135,15 +134,24 @@ mod tests {
             / runs.len();
 
         {
-            let mut output = File::create(format!("test_results/k_{}_n_{}_lambda_{}_epsilon_{}.csv", k,n,lambda,epsilon))?;
+            let mut output = File::create(format!(
+                "test_results/k_{}_lambda_{}_epsilon_{}.csv",
+                k, lambda, epsilon
+            ))?;
             writeln!(output, "estimate,actual")?;
             for (ams_estimate, actual) in runs {
                 writeln!(output, "{},{}", ams_estimate, actual)?;
             }
         }
 
-        println!("{} runs with ε = {}, λ = {} (buckets = {}, copies = {})", count, epsilon, lambda, s1, s2);
-        println!("median error {}, error proportion {}", median_error, error_proportion);
+        println!(
+            "{} runs with λ = {}, ε = {} (buckets = {}, copies = {})",
+            count, lambda, epsilon, s1, s2
+        );
+        println!(
+            "median error {}, error proportion {}",
+            median_error, error_proportion
+        );
         Ok(())
     }
 }
